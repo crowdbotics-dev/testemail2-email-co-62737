ad8fc987f5f5238b1c4c4f0ff70a51eeda9e8958
Crowdbotics <engineering@crowdbotics.com>
2023-02-10 21:34:31 +0000

    Initial commit.

---
+++ backend/testemail2_email_co_62737/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testemail2_email_co_62737 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testemail2_email_co_62737.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "testemail2-email-co-62737",
+  url: "https://testemail2-email-co-62737.botics.co",
+  api: "https://testemail2-email-co-62737.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
